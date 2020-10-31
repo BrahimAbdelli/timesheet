86d93a89c5c6c392541582484e202e239449b1b6
Brahim Abdelli <brahim.abdelli@esprit.tn>
2020-10-31 16:12:45 +0100

    Class Test

---
+++ src/test/java/tn/esprit/spring/EmployeTests.java
@@ -0,0 +1,68 @@
+package tn.esprit.spring;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.entities.Role;
+import tn.esprit.spring.services.ContratServiceImpl;
+import tn.esprit.spring.services.EmployeServiceImpl;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeTests {
+	
+	@Autowired
+	EmployeServiceImpl EmployeServiceImpl;
+	
+	
+	@Test
+	public void testAuthenticate() {
+
+		assertNull(EmployeServiceImpl.authenticate("cccc", "ccc"));
+
+	}
+	
+	
+	@Test
+	public void getEmployePrenomById() {
+		
+		assertNotNull(EmployeServiceImpl.getEmployePrenomById(3));
+
+	}
+
+	@Test
+	public void getNombreEmployeJPQL() {
+
+		assertEquals((EmployeServiceImpl.getNombreEmployeJPQL()),1);
+
+	}	
+	
+	@Test
+	public void getSalaireByEmployeIdJPQLTrue() {
+
+		assertTrue((EmployeServiceImpl.getSalaireByEmployeIdJPQL(3))<150000);
+
+	}
+	
+	@Test
+	public void getSalaireByEmployeIdJPQLFalse() {
+
+		assertFalse((EmployeServiceImpl.getSalaireByEmployeIdJPQL(3))>150000);
+
+	}	
+	
+	@Test
+	public void ajouterEmploye() {
+
+		Employe employe = new Employe( "nom","prenom","email@email.com", true, Role.ADMINISTRATEUR);
+		
+		assertNotNull(EmployeServiceImpl.addOrUpdateEmploye(employe));
+
+	}		
+}
